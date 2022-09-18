b2bcc83259f29785d15ac6a78e754c6cf5f4adc5
irieprincia <fannyprincia247@gmail.com>
2022-09-18 15:11:19 -0700

    Put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+*main - Prints a text
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+
+{
+
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
